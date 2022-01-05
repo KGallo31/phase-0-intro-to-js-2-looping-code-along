b43812b8b4a6e068ba56040208ffd0efe003a2e3
Kevin Gallo <Gallok731@gmail.com>
2022-01-04 22:19:27 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(arr,event)
+{
+    const newArr = [];
+    for(let i = 0;i<arr.length;i++)
+    {
+        let mes = "Thank you, "+ arr[i] + ", for the wonderful "+ event + " gift!";
+        newArr.push(mes);
+    }
+    return newArr;
+}
+function countDown(num)
+{
+    while(num>=0)
+    {
+        console.log(num);
+        num--;
+    }
+
+}
